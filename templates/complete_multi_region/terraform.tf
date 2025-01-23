90a819e90d286bc7ce14685ad641338f55bb1f8a
Ayodeji Bayo-Makinde <ayobaymak@gmail.com>
2025-01-23 14:12:28 +0100

    Update terraform.tf

--- templates/complete_multi_region/terraform.tf
+++ templates/complete_multi_region/terraform.tf
@@ -27,12 +27,18 @@ provider "azapi" {
 
 provider "azurerm" {
   skip_provider_registration = true
+  client_id                  = var.client_id
+  client_secret              = var.client_secret
+  tenant_id                  = var.tenant_id
   features {}
 }
 
 provider "azurerm" {
   skip_provider_registration = true
   alias                      = "management"
+  client_id                  = var.client_id
+  client_secret              = var.client_secret
+  tenant_id                  = var.tenant_id
   subscription_id            = var.subscription_id_management
   features {}
 }
@@ -40,6 +46,9 @@ provider "azurerm" {
 provider "azurerm" {
   skip_provider_registration = true
   alias                      = "connectivity"
+  client_id                  = var.client_id
+  client_secret              = var.client_secret
+  tenant_id                  = var.tenant_id
   subscription_id            = var.subscription_id_connectivity
   features {}
 }
